c5c24cbcfc52ea73258fa8d3085473e2dbbf0474
Alex Chin <alexanderchin@college.harvard.edu>
2019-01-10 12:04:27 -0500

    finised login form and passing up state

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import LogSign from "./LogSign";
+import LoginForm from "./LoginForm"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: ""
+    };
+  }
+
+  userHasAuthenticated = userid => {
+    this.setState({ id: userid });
+  }
+
   render() {
+    const childProps = {
+      id: this.state.id,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <LogSign text="Login" />
-          <LogSign text="Sign Up" />
+          <LoginForm childProps={childProps} />
         </header>
       </div>
     );
