b3a52013f23cbfde62e260a3e381fd21cd7a079e
Samuel Zhang <samzyao@gmail.com>
2019-01-10 12:10:20 -0500

    dashboard and routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import LogSign from "./LogSign";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
   render() {
@@ -9,8 +11,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <LogSign text="Login" />
-          <LogSign text="Sign Up" />
+          <Router>
+            <Link to="/dashboard">
+              <LogSign text="Login" />
+            </Link>
+            <Link to="/dashboard">
+              <LogSign text="Sign Up" />
+            </Link>
+
+            <Route path="/dashboard" component={Dashboard} />
+          </Router>
         </header>
       </div>
     );
