ae70dbaef6ffbeb483b6b95b4aefb65112302891
Samuel Zhang <samzyao@gmail.com>
2019-01-10 20:21:59 -0500

    layout

--- app/src/components/Dashboard.jsx
+++ app/src/components/Dashboard.jsx
@@ -1,15 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-export default class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      faq: []
-    };
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 4,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   }
+});
 
-  render() {
-    return <div>Hi</div>;
-  }
+function Dashboard(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={24}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
+
+// AutoGrid.propTypes = {
+//   classes: PropTypes.object.isRequired
+// };
+
+export default withStyles(styles)(Dashboard);
